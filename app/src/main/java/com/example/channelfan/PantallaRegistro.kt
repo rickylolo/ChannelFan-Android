07a2410d9746ce9aefe8e2d19cacaa040d85ea58
rickylolo <ricky_lolo29@hotmail.com>
2023-03-30 18:13:19 -0600

    ScrollView Register y OnClick Functions

--- app/src/main/java/com/example/channelfan/PantallaRegistro.kt
+++ app/src/main/java/com/example/channelfan/PantallaRegistro.kt
@@ -1,24 +1,25 @@
 package com.example.channelfan
 
 import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
-import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class PantallaRegistro : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_register)
 
         //Hide Toolbar
         supportActionBar?.hide()
 
+
         //Buttons to another activity
-        val btn_Register = findViewById<Button>(R.id.btn_Register)
-        btn_Register.setOnClickListener(object : View.OnClickListener {
+        val btn_Cancel = findViewById<Button>(R.id.btn_Cancel)
+        btn_Cancel.setOnClickListener(object : View.OnClickListener {
             override fun onClick(v: View?) {
-                val intent = Intent(this@MainActivity, PantallaRegistro::class.java)
+                val intent = Intent(this@PantallaRegistro, MainActivity::class.java)
                 startActivity(intent)
             }
         })
