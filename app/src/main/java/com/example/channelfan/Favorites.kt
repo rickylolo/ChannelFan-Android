991889974882011962e1c0636b13435a1ae93bb9
rickylolo <ricky_lolo29@hotmail.com>
2023-04-01 10:01:11 -0600

    Profile&Fav
    Avance de activity profile y favorites

--- app/src/main/java/com/example/channelfan/Favorites.kt
+++ app/src/main/java/com/example/channelfan/Favorites.kt
@@ -2,10 +2,20 @@ package com.example.channelfan
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.channelfan.adapter.FilmsAdapter
 
 class Favorites : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_favorites)
+        initRecyclerView()
+    }
+
+    fun initRecyclerView(){
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerFilms)
+        recyclerView.layoutManager = GridLayoutManager(this, 2)
+        recyclerView.adapter = FilmsAdapter(FilmsProvider.filmsList)
     }
 }
