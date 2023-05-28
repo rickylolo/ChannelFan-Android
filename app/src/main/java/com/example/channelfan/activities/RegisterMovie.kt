3e9efbd8d15513f3d641b1c6896cfd3cb9214075
Brenda Solis <75803157+Brenda3006@users.noreply.github.com>
2023-05-28 00:16:28 -0600

    Screens Img

--- app/src/main/java/com/example/channelfan/activities/RegisterMovie.kt
+++ app/src/main/java/com/example/channelfan/activities/RegisterMovie.kt
@@ -1,35 +1,53 @@
 package com.example.channelfan.activities
 
+import android.app.DatePickerDialog
+import android.app.Dialog
 import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
-import android.widget.ArrayAdapter
-import android.widget.Button
+import android.widget.*
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.fragment.app.DialogFragment
 import com.example.channelfan.R
-import android.widget.Spinner
-import android.widget.Toast
 import com.example.channelfan.databinding.ActivityRegisterMovieBinding
 import com.example.channelfan.endpoints.RetrofitClient
 import com.example.channelfan.models.ClassPelicula
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import java.util.*
 
 class RegisterMovie : AppCompatActivity() {
 
+    val pickMedia = registerForActivityResult(ActivityResultContracts.PickVisualMedia()){ uri->
+        if(uri!=null){
+            //imagen seleccionada
+                ivImg.setImageURI(uri)
+            Log.i("aris","Seleccion")
+        }else{
+            Log.i("aris","No Seleccionado")
+            //no imagen
+        }
+    }
+    lateinit var ivImg : ImageView
     lateinit var binding: ActivityRegisterMovieBinding
     var pelicula = ClassPelicula(null ,"","","",null,"","","","","","")
 
     private var spClasifiacion:Spinner?=null
+    private var cajaFecha:EditText?=null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityRegisterMovieBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        cajaFecha = findViewById(R.id.edFecha)
+        ivImg = findViewById(R.id.imgPelicula)
 
         //Hide Toolbar
         supportActionBar?.hide()
@@ -47,6 +65,8 @@ class RegisterMovie : AppCompatActivity() {
             val intent = Intent(this@RegisterMovie, MainActivity::class.java)
             startActivity(intent)
         }
+
+
 
         Log.d("Login", idUsuario.toString())
 
@@ -83,9 +103,47 @@ class RegisterMovie : AppCompatActivity() {
             }
         })
 
+        val btnFecha = findViewById<Button>(R.id.btnFecha)
+        btnFecha.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                val fecha = DatePickerFragment {year, month, dayOfMonth -> showResults(year, month, dayOfMonth) }
+                fecha.show(supportFragmentManager,"DatePicker")
+            }
+        })
+
+        val btnImagen = findViewById<Button>(R.id.btnAddImg)
+        btnImagen.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View?) {
+
+                /* ESTE SIRVE PARA LOS GIF
+                pickMedia.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.SingleMimeType("img/gif")))*/
+                pickMedia.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+            }
+        })
 
     }
 
+    private fun showResults(year: Int, month: Int, dayOfMonth: Int) {
+        var mes:String?=null
+        var dia:String?=null
+        var numint: Int?=0
+        if (dayOfMonth<=9){
+            dia = "0$dayOfMonth"
+        }else
+        {
+            dia = dayOfMonth.toString()
+        }
+
+            if (month<=8){
+                numint = month + 1
+                mes = "0$numint"
+            }else
+            {
+                numint = month + 1
+                mes = numint.toString()
+            }
+            cajaFecha?.setText("$year/$mes/$dia")
+    }
 
 
     fun validarCampos(): Boolean{
@@ -133,5 +191,21 @@ class RegisterMovie : AppCompatActivity() {
         this.pelicula.generos =null
         this.pelicula.clasificacion = ""
         this.pelicula.sinopsis = ""
+    }
+
+    class DatePickerFragment (val listener:(year: Int, month: Int, dayOfMonth: Int)->Unit): DialogFragment(), DatePickerDialog.OnDateSetListener{
+
+        override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+            val c= Calendar.getInstance()
+            var year = c.get(Calendar.YEAR)
+            var month = c.get(Calendar.MONTH)
+            var dayOfMonth = c.get(Calendar.DAY_OF_MONTH)
+            return DatePickerDialog(requireActivity(),this,year, month, dayOfMonth)
+        }
+
+        override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
+            listener(year, month, dayOfMonth)
+        }
+
     }
 }
